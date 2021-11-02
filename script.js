441099b50fbcad3fc1db3df03773675bf304f448
Matthew Wei <matthewwei35@gmail.com>
2021-11-01 17:25:30 -0700

    style(index/script): add sort buttons

--- script.js
+++ script.js
@@ -18,57 +18,33 @@ const passengers = data.map(p => {
   return el
 })
 
-data.sort((a, b) => {
-  if (a.fields.sex === 'female') {
-    return -1
-  }
-  return 1
-})
+// Let's loop over each passenger and set some styles
+function renderPassengers() {
+  passengers.forEach((p, i) => {
+    const { survived, sex, age } = data[i].fields
+    p.classList.add('passenger')
+    p.dataset.id = i
+    p.style.backgroundColor = '#000'
+  
+    p.style.opacity = survived === 'Yes' ? '100%' : '50%' 
+  
+    p.style.borderRadius = sex === 'female' ? '50%' : '0'
+  
+    p.style.backgroundColor = portColors[data[i].fields.embarked]
+  
+    if (age < 18) {
+      p.style.width = '10px'
+      p.style.height = '10px'
+      p.style.margin = '5px'
+    } else {
+      p.style.width = '20px'
+      p.style.height = '20px'
+      p.style.margin = 0
+    }
+  })
+}
 
-data.sort((a, b) => {
-  if (a.fields.survived === 'Yes') {
-    return -1
-  }
-  return 1
-})
-
-data.sort((a, b) => {
-  if (a.fields.embarked < b.fields.embarked) {
-    return -1
-  } else if (a.fields.embarked > b.fields.embarked) {
-    return 1
-  }
-  return 0
-})
-
-data.sort((a, b) => {
-  if (a.fields.age < 18) {
-    return 1
-  }
-  return -1
-})
-
-// Let's loop over each passenger and set some styles 
-passengers.forEach((p, i) => {
-  const { survived, sex, age } = data[i].fields
-  p.classList.add('passenger')
-  p.dataset.id = i
-  p.style.width = '20px'
-  p.style.height = '20px'
-  p.style.backgroundColor = '#000'
-
-  p.style.opacity = survived === 'Yes' ? '100%' : '50%' 
-
-  p.style.borderRadius = sex === 'female' ? '50%' : '0'
-
-  p.style.backgroundColor = portColors[data[i].fields.embarked]
-
-  if (age < 18) {
-    p.style.width = '10px'
-    p.style.height = '10px'
-    p.style.margin = '5px'
-  }
-})
+renderPassengers()
 
 const titanicEmbarked = document.querySelector('#titanic-embarked')
 
@@ -119,5 +95,66 @@ document.body.addEventListener('mouseover', (e) => {
 document.body.addEventListener('mouseout', (e) => {
   if (e.target.matches('.passenger')) {
     passengerDetails.style.display = 'none'
+  }
+})
+
+function sortSurvived() {
+  data.sort((a, b) => {
+    if (a.fields.survived === 'Yes') {
+      return -1
+    }
+    return 1
+  })
+}
+
+function sortAge() {
+  data.sort((a, b) => {
+    return a.fields.age - b.fields.age
+  })
+}
+
+function sortSex() {
+  data.sort((a, b) => {
+    if (a.fields.sex === 'female') {
+      return -1
+    }
+    return 1
+  })
+}
+
+function sortFare() {
+  data.sort((a, b) => {
+    return a.fields.fare - b.fields.fare
+  })
+}
+
+function sortEmbarked() {
+  data.sort((a, b) => {
+    if (a.fields.embarked < b.fields.embarked) {
+      return -1
+    } else if (a.fields.embarked > b.fields.embarked) {
+      return 1
+    }
+    return 0
+  })
+}
+
+document.body.addEventListener('click', (e) => {
+  console.log(e.target)
+  if (e.target.matches('.sort-by-survived')) {
+    sortSurvived()
+    renderPassengers()
+  } else if (e.target.matches('.sort-by-age')) {
+    sortAge()
+    renderPassengers()
+  } else if (e.target.matches('.sort-by-sex')) {
+    sortSex()
+    renderPassengers()
+  } else if (e.target.matches('.sort-by-fare')) {
+    sortFare()
+    renderPassengers()
+  } else if (e.target.matches('.sort-by-embarked')) {
+    sortEmbarked()
+    renderPassengers()
   }
 })
